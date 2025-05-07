500afe7119a1767ab8990787b02c8db60045ca32
Saba Pachulia <sabapachulia@omegas-MacBook-Pro.local>
2025-05-07 10:13:03 +0400

    debugging

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import App from "./App.tsx";
 import ReactDOM from "react-dom/client";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import "./index.css";
@@ -9,9 +9,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
