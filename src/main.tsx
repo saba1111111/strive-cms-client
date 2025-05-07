ebaa6290474bf2eba400c974f331274a7b9c8acb
Saba Pachulia <sabapachulia@omegas-MacBook-Pro.local>
2025-05-07 10:20:08 +0400

    debugging

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import App from "./App.tsx";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
