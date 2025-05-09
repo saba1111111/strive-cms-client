6bd2181c95047415e88d803eee68b5a2a21dc760
Saba Pachulia <sabapachulia@omegas-MacBook-Pro.local>
2025-05-09 10:57:39 +0400

    debugging

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || "/",
+  base: "/strive-cms-client/",
   plugins: [react()],
-  build: { outDir: "docs" },
 });
